6720c9d5bdfea7e27ee03abaec5391c35cf281f0
Michael Peer <44434337+Michael-Peer@users.noreply.github.com>
2020-04-19 18:51:24 +0300

    Delete RecyclerViewContainer.kt

--- app/src/main/java/com/example/coronaapp/RecyclerViewContainer.kt
+++
@@ -1,7 +0,0 @@
-package com.example.coronaapp
-
-//package com.example.coronaapp
-//
-//import com.example.coronaapp.network.Property
-//
-//class RecyclerViewContainer (var property: Property?, var isHeader: Boolean, var headerTitle: String?)
